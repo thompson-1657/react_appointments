a9729b96b85d11a0f70f174771f4bb03c981350b
Rachel Thompson <thompson.1657@gmail.com>
2021-08-29 17:11:21 -0500

    add appt form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { BiArchive } from "react-icons/bi";
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
    <h1 className="text-5xl"><BiArchive className="inline-block text-red-600 align-top" /> Your Appointments</h1>
+   <AddAppointment />
    <Search />
     </div>
   );
